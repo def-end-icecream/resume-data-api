e4b8972edabc82dc73795dbfa2d983265b455ea6
sean <maherseanmilo@gmail.com>
2021-03-04 15:54:52 -0600

    changes photos route to pokemon

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/photos" => "photos#index"
+    get "/pokemon" => "pokemon#index"
   end
 
   # Chris: Test Code!
