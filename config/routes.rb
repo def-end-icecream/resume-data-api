5e02a9fef9dbae52b888163a02afb733e74cd192
chris_rezac <rezacchristopher@gmail.com>
2021-03-08 14:10:18 -0500

    adds student authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+
+  namespace :api do
+    #student sign up and login
+    post "/students" => "students#create"
+    post "/sessions" => "sessions#create"
+  end
 end
