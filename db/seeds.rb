e4c7d1f9623359dab8fd54afe6d5f57192353422
sean <maherseanmilo@gmail.com>
2021-03-08 13:51:45 -0600

    adds seed data for educations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# educations = Education.create!([
+#   {
+#     start_date: "2021-01-01",
+#     end_date: "2021-01-02",
+#     degree: "Bachelor's",
+#     university_name: "Somewhere USA",
+#     details: "Whooo, i'm a college graduate!"
+#   },
+#   {
+#     start_date: "2021-01-03",
+#     end_date: "2021-01-04",
+#     degree: "Bachelor's",
+#     university_name: "Somewhere USA",
+#     details: "This was so hard..."
+#   },
+#   {
+#     start_date: "2021-01-05",
+#     end_date: "2021-01-06",
+#     degree: "Bachelor's",
+#     university_name: "Somewhere USA",
+#     details: "Hot damn, would ya look at this paper?"
+#   },
+# ])
