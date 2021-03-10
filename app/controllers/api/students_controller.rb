92c9baca972e540664a6dc1b1194f3faef89eba1
chris_rezac <rezacchristopher@gmail.com>
2021-03-10 14:19:16 -0500

    adds student show partials

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -7,6 +7,14 @@ class Api::StudentsController < ApplicationController
     @student = Student.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
+      phone_number: params[:phone_number],
+      bio: params[:bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      personal_website_url: params[:personal_website_url],
+      resume_url: params[:resume_url],
+      github_url: params[:github_url],
+      image_url: params[:image_url],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
